0204b911648f76eff9975f0f3622216272267754
emiliepnt <emiliepnt9@gmail.com>
2020-05-14 18:25:18 +0200

    création des templates pour les pages Matières et Etudiants

--- Vue/template.php
+++ Vue/template.php
@@ -26,7 +26,7 @@
   <div class="collapse navbar-collapse">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="indexx.php">Accueil</a>
+        <a class="nav-link" href="index.php">Accueil</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="matieres.php">Matières</a>
