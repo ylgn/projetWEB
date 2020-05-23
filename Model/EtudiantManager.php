dba7d211ff627cc3c98f605b79e4a17cd6fd556a
tpiganeau <thais.piganeau@dauphine.eu>
2020-05-23 18:25:49 +0200

    Update EtudiantManager.php
    Fonction createCandidat
    Fonction createEtudiant

--- Model/EtudiantManager.php
+++ Model/EtudiantManager.php
@@ -28,9 +28,16 @@ function searchEtudiant($adresse){
 	return $resultat;
 }
 
-function createEtudiant($nom,$prenom,$adresse,$date_naissance,$note_maths,$note_info,$note_anglais,$note_moyenne,$statut,$parcours,$lettre_motivation){
+function createCandidat($nom,$prenom,$adresse,$date_naissance,$note_maths,$note_info,$note_anglais,$note_moyenne,$statut,$parcours,$lettre_motivation){
 	include("ConnexionBase.php");
 	$querry = "INSERT INTO `ETUDIANT` (`ID`, `nom`, `prenom`, `adresse`, `date_naissance`, `note_maths`, `note_info`, `note_anglais`, `note_moyenne`, `statut`, `parcours`, `lettre_motivation`, `IDSt`, `Nom_societe`, `Site`,`Photo`,`CV`,`Page`) VALUES (NULL, '".$nom."', '".$prenom."', '".$adresse."', '".$date_naissance."', '".$note_maths."', '".$note_info."', '".$note_anglais."', '".$note_moyenne."', '".$statut."', '".$parcours."', '".$lettre_motivation."', NULL, NULL, NULL,NULL,NULL,NULL)";
+	$prepared_querry = $connect->prepare($querry);
+    $prepared_querry->execute();
+}
+
+function createEtudiant($nom,$prenom,$adresse,$date_naissance,$note_maths,$note_info,$note_anglais,$note_moyenne,$statut,$parcours,$lettre_motivation,$idst,$nom_societe,$site,$photo){
+	include("ConnexionBase.php");
+	$querry = "INSERT INTO `ETUDIANT` (`ID`, `nom`, `prenom`, `adresse`, `date_naissance`, `note_maths`, `note_info`, `note_anglais`, `note_moyenne`, `statut`, `parcours`, `lettre_motivation`, `IDSt`, `Nom_societe`, `Site`,`Photo`,`CV`,`Page`) VALUES (NULL, '".$nom."', '".$prenom."', '".$adresse."', '".$date_naissance."', '".$note_maths."', '".$note_info."', '".$note_anglais."', '".$note_moyenne."', '".$statut."', '".$parcours."', '".$lettre_motivation."', '".$idst."', '".$nom_societe."', '".$site."','".$photo."',NULL,NULL)";
 	$prepared_querry = $connect->prepare($querry);
     $prepared_querry->execute();
 }
